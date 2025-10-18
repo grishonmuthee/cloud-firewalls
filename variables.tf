61e37f17d90b3fefad46f62da36e67c365c1f25a
Grishon Muthee <70142037+grishonmuthee@users.noreply.github.com>
2025-10-18 05:48:01 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
 variable "aws_region" {
-  description = "AWS region to deploy in"
-  default     = "us-east-1"
+  default = "us-east-1"
+}
+
+variable "my_ip" {
+  description = "Your public IP address for SSH access"
+  default     = "0.0.0.0/0" # replace with your IP later (e.g., 203.0.113.15/32)
 }
