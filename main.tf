d39a37b230df933e322e013b4bdc4979527e99c3
Grishon Muthee <70142037+grishonmuthee@users.noreply.github.com>
2025-10-18 05:42:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,16 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "firewall-vpc" }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = { Name = "public-subnet" }
+}
